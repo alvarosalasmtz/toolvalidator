f9c5eaa149fd376b3e75356054ab95e499f86bf1
Alvaro Salas <alvaro.salas@interware.com.mx>
2016-04-08 19:18:30 -0500

    coding #3 se empieza con el flujo de validacion

---
+++ test/services/FindServiceTest.js
@@ -0,0 +1,15 @@
+var chai = require('chai');
+chai.should();
+var assert = chai.assert
+
+var findService = require('../../lib/services/FindService');
+
+describe('FindService', () => {
+
+    it('find one item in list', () => {
+        var list = [{id:'a', desc:'text'},{id:'b', desc:'text'},{id:'1', desc:'number'},{id:'2', desc:'number'}];
+        var find = findService.findOneInList({'id':'a'}, list);
+        assert(find.id == 'a');
+        assert(find.desc == 'text');
+    });
+});
