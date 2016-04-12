62ee551990bdb4e7b4648d10ccaf89d40649a4f1
Alvaro Salas <alvaro.salas@interware.com.mx>
2016-04-11 19:31:56 -0500

    coding #3 se empieza con el flujo de validacion y se agrega reglas

--- test/services/FindServiceTest.js
+++ test/services/FindServiceTest.js
@@ -8,7 +8,7 @@ describe('FindService', () => {
 
     it('find one item in list', () => {
         var list = [{id:'a', desc:'text'},{id:'b', desc:'text'},{id:'1', desc:'number'},{id:'2', desc:'number'}];
-        var find = findService.findOneInList({'id':'a'}, list);
+        var find = findService.findOneInList({id:'a'}, list);
         assert(find.id == 'a');
         assert(find.desc == 'text');
     });
